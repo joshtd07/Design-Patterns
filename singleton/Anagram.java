d89e0d650bc8340f9e438e5681ba26c226373a20
joshtd07 <111777481+joshtd07@users.noreply.github.com>
2023-10-01 23:35:15 -0400

    Add files via upload

---
+++ singleton/Anagram.java
@@ -0,0 +1,22 @@
+package singleton;
+import java.util.ArrayList;
+
+public class Anagram {
+    
+    private String question;
+    private ArrayList<String> answers;
+
+    public Anagram(String question, ArrayList<String> answers){
+        this.question=question;
+        this.answers=answers;
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public boolean isCorrect(String userAnswer){
+        return answers.contains(userAnswer.toLowerCase());
+    }
+
+}
