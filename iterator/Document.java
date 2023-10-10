5b534f6ffb08f559659a8b79e2534322f86da7ef
joshtd07 <jdoyon@email.sc.edu>
2023-10-09 22:12:14 -0400

    Iterator

--- iterator/Document.java
+++ iterator/Document.java
@@ -2,22 +2,41 @@ package iterator;
 
 import java.util.*;
 
+/**
+ * The Document class represents a document with a title and supports undo and redo operations on changes made to the document.
+ */
 public class Document {
     private String title;
     private Stack<String> undoStack = new Stack<String>();
     private Stack<String> redoStack = new Stack<String>();
 
+    /**
+     * Constructs a new Document object with the specified title.
+     *
+     * @param title The title of the document.
+     */
     public Document(String title) {
         this.title = title;
     }
 
+    /**
+     * Makes a change to the document and pushes it onto the undo stack.
+     *
+     * @param change The change to be made to the document.
+     * @return The change that was made.
+     */
     public String makeChange(String change) {
         undoStack.push(change);
         return change;
     }
 
+    /**
+     * Undoes the last change made to the document.
+     *
+     * @return A message indicating the change that was undone, or an empty string if there are no changes to undo.
+     */
     public String undoChange() {
-        if(!undoStack.isEmpty() && undoStack.peek() != null) {
+        if (!undoStack.isEmpty() && undoStack.peek() != null) {
             String change = undoStack.pop();
             redoStack.push(change);
             return "Undoing change: " + change;
@@ -25,8 +44,13 @@ public class Document {
         return " ";
     }
 
+    /**
+     * Redoes the last undone change to the document.
+     *
+     * @return A message indicating the change that was redone, or an empty string if there are no changes to redo.
+     */
     public String redoChange() {
-        if(!redoStack.isEmpty() && redoStack.pop() != null) {
+        if (!redoStack.isEmpty() && redoStack.pop() != null) {
             String change = redoStack.pop();
             undoStack.push(change);
             return "Redoing change: " + change;
@@ -34,19 +58,34 @@ public class Document {
         return " ";
     }
 
+    /**
+     * Checks if there are changes that can be undone.
+     *
+     * @return true if there are changes that can be undone, false otherwise.
+     */
     public boolean canUndo() {
         return !undoStack.isEmpty() && undoStack.peek() != null;
     }
 
+    /**
+     * Checks if there are changes that can be redone.
+     *
+     * @return true if there are changes that can be redone, false otherwise.
+     */
     public boolean canRedo() {
         return !redoStack.isEmpty() && redoStack.peek() != null;
     }
 
+    /**
+     * Gets an iterator for traversing the undo stack.
+     *
+     * @return A StackIterator for the undo stack.
+     */
     public StackIterator getUndoIterator() {
         return new StackIterator(undoStack);
     }
 
-    public StackIterator getRedoIterator() {
-        return new StackIterator(redoStack);
-    }
-}
+    /**
+     * Gets an iterator for traversing the redo stack.
+     *
+     * @return A StackIterator for the
--- iterator/StackIterator.java
+++ iterator/StackIterator.java
@@ -2,21 +2,39 @@ package iterator;
 
 import java.util.*;
 
+/**
+ * The StackIterator class provides an iterator for traversing a Stack of strings.
+ */
 public class StackIterator {
     private Stack<String> items = new Stack<String>();
     private int position;
     Iterator<String> iterator = items.iterator();
 
+    /**
+     * Constructs a new StackIterator object for the given Stack of strings.
+     *
+     * @param items The Stack of strings to be iterated.
+     */
     public StackIterator(Stack<String> items) {
         this.items = items;
     }
 
+    /**
+     * Checks if there are more elements in the Stack to iterate over.
+     *
+     * @return true if there are more elements to iterate over, false otherwise.
+     */
     public boolean hasNext() {
         return position < items.size() && items.get(position) != null;
     }
 
+    /**
+     * Returns the next element in the Stack and advances the iterator position.
+     *
+     * @return The next element in the Stack, or null if there are no more elements to iterate.
+     */
     public String next() {
-        if(hasNext()) {
+        if (hasNext()) {
             return items.get(position++);
         }
         return null;
